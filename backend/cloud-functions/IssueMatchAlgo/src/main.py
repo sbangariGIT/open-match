c82fb83870aafcda8d1e27f268cf3d45ac8b52ad
sbangariGIT <bangarisairohan@gmail.com>
2024-11-14 23:33:14 -0800

    add slack bot, that sends logs

--- backend/cloud-functions/IssueMatchAlgo/src/main.py
+++ backend/cloud-functions/IssueMatchAlgo/src/main.py
@@ -2,18 +2,22 @@ import time
 import functions_framework
 from .models.userprofile import UserProfile
 from .services.db import DB
+from .logging.logger import central_logger
 
 
 def process_request(data):
     try:
         userProfile = UserProfile(**data)
+        central_logger.notify_slack(f"YAY!!! {userProfile.firstName}, {userProfile.email} has requested to use our service")
         embedding = userProfile.get_query_embedding()
         result = {"result": embedding}
         result = DB.get_k_nearest_issues(embedding, k=4)
         return result, 200
     except ValueError as e:
+        central_logger.notify_slack(f"OHHH NOOO!!! An Error Occured {str(e)}")
         return {"error": str(e)}, 200
     except Exception as e:
+        central_logger.notify_slack(f"OHHH NOOO!!! An Error Occured {str(e)}")
         return {"error": str(e)}, 200
 
 @functions_framework.http
@@ -39,5 +43,4 @@ def get_issue_match(request):
             result.update({'request_process_time': end_time - start_time})
             return result, status_code, headers
         except Exception as e:
-            print(e)
             return 'Not a valid JSON', 400, headers
