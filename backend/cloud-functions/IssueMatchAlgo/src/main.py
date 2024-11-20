0a5a2810bfaf803a45963c618cf8da071b0ef92b
sbangariGIT <bangarisairohan@gmail.com>
2024-11-19 23:41:55 -0800

    add log levels

--- backend/cloud-functions/IssueMatchAlgo/src/main.py
+++ backend/cloud-functions/IssueMatchAlgo/src/main.py
@@ -8,16 +8,17 @@ from .logging.logger import central_logger
 def process_request(data):
     try:
         userProfile = UserProfile(**data)
-        central_logger.notify_slack(f"YAY!!! {userProfile.firstName}, {userProfile.email} has requested to use our service")
+        central_logger.info(f"YAY!!! {userProfile.firstName}, {userProfile.email} has requested to use our service")
         embedding = userProfile.get_query_embedding()
         result = {"result": embedding}
         result = DB.get_k_nearest_issues(embedding, k=4)
+        central_logger.info(f"UFF!!! Done proccessing request for {userProfile.firstName}, {userProfile.email}")
         return result, 200
     except ValueError as e:
-        central_logger.notify_slack(f"OHHH NOOO!!! An Error Occured {str(e)}")
+        central_logger.severe(f"OHHH NOOO!!! An Error Occured {str(e)}")
         return {"error": str(e)}, 200
     except Exception as e:
-        central_logger.notify_slack(f"OHHH NOOO!!! An Error Occured {str(e)}")
+        central_logger.severe(f"OHHH NOOO!!! An Error Occured {str(e)}")
         return {"error": str(e)}, 200
 
 @functions_framework.http
@@ -41,6 +42,8 @@ def get_issue_match(request):
             result, status_code = process_request(data=payload)
             end_time = time.time()
             result.update({'request_process_time': end_time - start_time})
+            central_logger.info(f"request process time took {end_time - start_time}")
             return result, status_code, headers
         except Exception as e:
+            central_logger.severe(f"OHHH NOOO!!! An Error Occured {str(e)}")
             return 'Not a valid JSON', 400, headers
